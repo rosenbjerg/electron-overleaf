bd994b49719c1941fcd1deff2bfc230a14cfae04
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-10-10 20:46:03 +0200

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1280, height: 870,
+	webPreferences: {
+	  nodeIntegration: false
+	}});
+  mainWindow.loadURL('https://v2.overleaf.com/project');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
